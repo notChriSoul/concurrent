410852eeb4401b8878a2d9faf9f6254b62a73bb3
Administrator <Administrator@HPG5KD>
2024-03-11 16:43:34 +0100

    Dodaj pliki projektów.

---
+++ concurrent/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace concurrent
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
