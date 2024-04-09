520119d3b29556c1124dbcd777871972e1dbf15e
247649 Dusza Krzysztof <247649@edu.p.lodz.pl>
2024-04-09 13:24:07 +0200

    refactor do drugiego zadania po części robiony

---
+++ TestyJednostkowe/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TestyJednostkowe
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
